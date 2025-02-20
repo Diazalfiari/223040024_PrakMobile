a5022075b19bc11322edc829d0f6eeb97c7a1bc2
Diazalfiari <diazalfiari19@gmail.com>
2025-02-20 13:39:27 +0700

    Mengerjakan latihan `1 sampe 7 sesi2

---
+++ Latihan/Sesi 2/src/Latihan3TipeData&Var.kt
@@ -0,0 +1,14 @@
+//fun main() {
+//    var nama = "Kotlin"
+//    println(nama) // output: Kotlin
+//}
+
+//fun main(args: Array<String>){
+//    var nama2: String = "diaz"
+//    println(nama2) //output diaz
+//}
+
+fun main() {
+    var umur: Int = 25
+    println(umur) //Output: 25
+}
